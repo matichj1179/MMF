d6f4a45cdb8a270886fca6cd52aae583c1735954
matichj1179 <101526906+matichj1179@users.noreply.github.com>
2024-02-09 14:43:12 +1300

    added yes no check function

---
+++ F_01_yes_no_v1.py
@@ -0,0 +1,22 @@
+# functions go here
+
+# Checks user answer yes / no to a question
+def yes_no(question):
+    valid = False
+    while not valid:
+        response = input(question).lower()
+
+        if response == "yes" or response == "y":
+            return "yes"
+
+        elif response == "no" or response == "n":
+            return "no"
+
+        else:
+            print("please answer yes / no")
+
+
+# main routine goes here
+show_instructions = yes_no("Do you want to see the instructions? ")
+
+print(show_instructions)
