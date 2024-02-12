baba24e6746ad5b2bbadf6e795e51cfd94c67425
matichj1179 <101526906+matichj1179@users.noreply.github.com>
2024-02-13 09:59:24 +1300

    added base code

---
+++ 00_base_v01.py
@@ -0,0 +1,47 @@
+# functions go here
+
+# checks users have entered a yes / no question
+def yes_no(question):
+    valid = False
+    while not valid:
+        response = input(question).lower()
+
+        if response == "yes" or response == "y":
+            return "yes"
+
+        elif response == "no" or response == "n":
+            return "no"
+
+        else:
+            print("please answer yes / no")
+
+
+# main routine goes here
+
+# set max amount of tickets here
+MAX_TICKETS = 3
+tickets_sold = 0
+
+# ask user if they want to see the instructions
+show_instructions = yes_no("Do you want to see the instructions? ")
+
+if show_instructions == "yes":
+    print("instructions go here")
+
+
+print()
+
+# loop to sell tickets
+while tickets_sold < MAX_TICKETS:
+    name = input("Please enter your name or 'xxx' to quit ")
+
+    if name == 'xxx':
+        break
+
+    tickets_sold += 1
+
+# output number of tickets sold
+if tickets_sold == MAX_TICKETS:
+    print("Congratulations you have sold all the tickets")
+else:
+    print(f"You have sold {tickets_sold} ticket/s. There is {MAX_TICKETS - tickets_sold} ticket/s remaining")
